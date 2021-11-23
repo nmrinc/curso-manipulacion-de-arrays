96efc89e13755587162d10f8064b10b63985f81a
Erick Avila <eavila@interware.com.mx>
2021-11-23 17:35:28 -0600

    [update] Solution to 'every\ challenge

--- server/09-every.js
+++ server/09-every.js
@@ -1,33 +1,36 @@
-const numbers = [1,30,39,29,10,13];
-
+const numbers = [1, 30, 39, 29, 10, 13];
 
 let rta = true;
 for (let index = 0; index < numbers.length; index++) {
-    const element = numbers[index];
-    if (element >= 40) {
-        rta = false
-    }
+	const element = numbers[index];
+	if (element >= 40) {
+		rta = false;
+	}
 }
 console.log('for', rta);
-const rta2 = numbers.every(item => item <= 40)
+const rta2 = numbers.every((item) => item <= 40);
 console.log('rta2', rta2);
 
-
 const team = [
-  {
-    name: "Nicolas",
-    age: 12,
-  },
-  {
-    name: "Andrea",
-    age: 8,
-  },
-  {
-    name: "Zulema",
-    age: 2,
-  },
-  {
-    name: "Santiago",
-    age: 18,
-  },
+	{
+		name: 'Nicolas',
+		age: 12,
+	},
+	{
+		name: 'Andrea',
+		age: 8,
+	},
+	{
+		name: 'Zulema',
+		age: 2,
+	},
+	{
+		name: 'Santiago',
+		age: 18,
+	},
 ];
+
+const challenge = team.map((item) => item.age).every((item) => item <= 15);
+console.log('====================================');
+console.log(challenge);
+console.log('====================================');
